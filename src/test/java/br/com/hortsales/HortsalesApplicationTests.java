c61cda640782f2de40b2ec22eda0d2b9c6ad9378
luiz <luizhenrique201387@gmail.com>
2022-08-16 22:48:39 -0300

    Projeto com dependencias.

---
+++ src/test/java/br/com/hortsales/HortsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hortsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
